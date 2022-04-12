2052154c16fcbb7219cae9d9aa5df5d04b001856
nieyanbing <2091220912@qq.com>
2022-04-12 13:41:11 +0800

    炮倍解锁

---
+++ 炮倍解锁/config_cannon_unlock.lua
@@ -0,0 +1,479 @@
+return {
+    ["39"] = {
+        nextCannonLv = 10000,
+        expMultiple = 1.3,
+        reward = {{
+            ["1001"] = 300000
+        }},
+        requireItem = {{
+            ["5001"] = 2500
+        }},
+        cannonLv = 9000
+    },
+    ["27"] = {
+        nextCannonLv = 1500,
+        expMultiple = 1.0,
+        reward = {{
+            ["1001"] = 90000
+        }},
+        requireItem = {{
+            ["5001"] = 150
+        }},
+        cannonLv = 1000
+    },
+    ["37"] = {
+        nextCannonLv = 8000,
+        expMultiple = 1.3,
+        reward = {{
+            ["1001"] = 300000
+        }},
+        requireItem = {{
+            ["5001"] = 1500
+        }},
+        cannonLv = 7000
+    },
+    ["25"] = {
+        nextCannonLv = 900,
+        expMultiple = 1.0,
+        reward = {{
+            ["1001"] = 70000
+        }},
+        requireItem = {{
+            ["5001"] = 90
+        }},
+        cannonLv = 800
+    },
+    ["35"] = {
+        nextCannonLv = 6000,
+        expMultiple = 1.2,
+        reward = {{
+            ["1001"] = 250000
+        }},
+        requireItem = {{
+            ["5001"] = 1000
+        }},
+        cannonLv = 5000
+    },
+    ["13"] = {
+        nextCannonLv = 100,
+        expMultiple = 1.0,
+        reward = {{
+            ["1001"] = 5000
+        }, {
+            ["4022"] = 1
+        }},
+        requireItem = {{
+            ["5001"] = 15
+        }},
+        cannonLv = 90
+    },
+    ["23"] = {
+        nextCannonLv = 700,
+        expMultiple = 1.0,
+        reward = {{
+            ["1001"] = 55000
+        }, {
+            ["4044"] = 1
+        }},
+        requireItem = {{
+            ["5001"] = 70
+        }},
+        cannonLv = 600
+    },
+    ["11"] = {
+        nextCannonLv = 80,
+        expMultiple = 1.0,
+        reward = {{
+            ["1001"] = 4000
+        }},
+        requireItem = {{
+            ["5001"] = 13
+        }},
+        cannonLv = 70
+    },
+    ["21"] = {
+        nextCannonLv = 500,
+        expMultiple = 1.0,
+        reward = {{
+            ["1001"] = 45000
+        }, {
+            ["4043"] = 1
+        }},
+        requireItem = {{
+            ["5001"] = 50
+        }},
+        cannonLv = 450
+    },
+    ["9"] = {
+        nextCannonLv = 60,
+        expMultiple = 1.0,
+        reward = {{
+            ["1001"] = 3500
+        }},
+        requireItem = {{
+            ["5001"] = 11
+        }},
+        cannonLv = 50
+    },
+    ["17"] = {
+        nextCannonLv = 300,
+        expMultiple = 1.0,
+        reward = {{
+            ["1001"] = 25000
+        }, {
+            ["4041"] = 1
+        }},
+        requireItem = {{
+            ["5001"] = 30
+        }},
+        cannonLv = 250
+    },
+    ["6"] = {
+        nextCannonLv = 40,
+        expMultiple = 1.0,
+        reward = {{
+            ["1001"] = 2500
+        }, {
+            ["4018"] = 1
+        }},
+        requireItem = {{
+            ["5001"] = 8
+        }},
+        cannonLv = 35
+    },
+    ["29"] = {
+        nextCannonLv = 2500,
+        expMultiple = 1.1,
+        reward = {{
+            ["1001"] = 100000
+        }},
+        requireItem = {{
+            ["5001"] = 250
+        }},
+        cannonLv = 2000
+    },
+    ["8"] = {
+        nextCannonLv = 50,
+        expMultiple = 1.0,
+        reward = {{
+            ["1001"] = 3000
+        }, {
+            ["4021"] = 1
+        }},
+        requireItem = {{
+            ["5001"] = 10
+        }},
+        cannonLv = 45
+    },
+    ["7"] = {
+        nextCannonLv = 45,
+        expMultiple = 1.0,
+        reward = {{
+            ["1001"] = 3000
+        }},
+        requireItem = {{
+            ["5001"] = 9
+        }},
+        cannonLv = 40
+    },
+    ["2"] = {
+        nextCannonLv = 20,
+        expMultiple = 1.0,
+        reward = {{
+            ["1001"] = 1000
+        }, {
+            ["4020"] = 1
+        }},
+        requireItem = {{
+            ["5001"] = 4
+        }},
+        cannonLv = 15
+    },
+    ["1"] = {
+        nextCannonLv = 15,
+        expMultiple = 1.0,
+        reward = {{
+            ["1001"] = 800
+        }, {
+            ["4017"] = 1
+        }},
+        requireItem = {{
+            ["5001"] = 3
+        }},
+        cannonLv = 10
+    },
+    ["4"] = {
+        nextCannonLv = 30,
+        expMultiple = 1.0,
+        reward = {{
+            ["1001"] = 1500
+        }, {
+            ["6008"] = 1
+        }},
+        requireItem = {{
+            ["5001"] = 6
+        }},
+        cannonLv = 25
+    },
+    ["3"] = {
+        nextCannonLv = 25,
+        expMultiple = 1.0,
+        reward = {{
+            ["1001"] = 1200
+        }},
+        requireItem = {{
+            ["5001"] = 5
+        }},
+        cannonLv = 20
+    },
+    ["38"] = {
+        nextCannonLv = 9000,
+        expMultiple = 1.3,
+        reward = {{
+            ["1001"] = 300000
+        }},
+        requireItem = {{
+            ["5001"] = 2000
+        }},
+        cannonLv = 8000
+    },
+    ["28"] = {
+        nextCannonLv = 2000,
+        expMultiple = 1.1,
+        reward = {{
+            ["1001"] = 95000
+        }},
+        requireItem = {{
+            ["5001"] = 200
+        }},
+        cannonLv = 1500
+    },
+    ["36"] = {
+        nextCannonLv = 7000,
+        expMultiple = 1.3,
+        reward = {{
+            ["1001"] = 250000
+        }},
+        requireItem = {{
+            ["5001"] = 1200
+        }},
+        cannonLv = 6000
+    },
+    ["26"] = {
+        nextCannonLv = 1000,
+        expMultiple = 1.0,
+        reward = {{
+            ["1001"] = 80000
+        }, {
+            ["4045"] = 1
+        }},
+        requireItem = {{
+            ["5001"] = 100
+        }},
+        cannonLv = 900
+    },
+    ["24"] = {
+        nextCannonLv = 800,
+        expMultiple = 1.0,
+        reward = {{
+            ["1001"] = 60000
+        }},
+        requireItem = {{
+            ["5001"] = 80
+        }},
+        cannonLv = 700
+    },
+    ["14"] = {
+        nextCannonLv = 150,
+        expMultiple = 1.0,
+        reward = {{
+            ["1001"] = 10000
+        }},
+        requireItem = {{
+            ["5001"] = 20
+        }},
+        cannonLv = 100
+    },
+    ["22"] = {
+        nextCannonLv = 600,
+        expMultiple = 1.0,
+        reward = {{
+            ["1001"] = 50000
+        }},
+        requireItem = {{
+            ["5001"] = 60
+        }},
+        cannonLv = 500
+    },
+    ["12"] = {
+        nextCannonLv = 90,
+        expMultiple = 1.0,
+        reward = {{
+            ["1001"] = 4000
+        }},
+        requireItem = {{
+            ["5001"] = 14
+        }},
+        cannonLv = 80
+    },
+    ["10"] = {
+        nextCannonLv = 70,
+        expMultiple = 1.0,
+        reward = {{
+            ["1001"] = 3500
+        }, {
+            ["4019"] = 1
+        }},
+        requireItem = {{
+            ["5001"] = 12
+        }},
+        cannonLv = 60
+    },
+    ["30"] = {
+        nextCannonLv = 3000,
+        expMultiple = 1.1,
+        reward = {{
+            ["1001"] = 100000
+        }},
+        requireItem = {{
+            ["5001"] = 300
+        }},
+        cannonLv = 2500
+    },
+    ["20"] = {
+        nextCannonLv = 450,
+        expMultiple = 1.0,
+        reward = {{
+            ["1001"] = 40000
+        }},
+        requireItem = {{
+            ["5001"] = 45
+        }},
+        cannonLv = 400
+    },
+    ["16"] = {
+        nextCannonLv = 250,
+        expMultiple = 1.0,
+        reward = {{
+            ["1001"] = 20000
+        }},
+        requireItem = {{
+            ["5001"] = 28
+        }},
+        cannonLv = 200
+    },
+    ["40"] = {
+        nextCannonLv = 20000,
+        expMultiple = 1.4,
+        reward = {{
+            ["1001"] = 400000
+        }},
+        requireItem = {{
+            ["5001"] = 3000
+        }},
+        cannonLv = 10000
+    },
+    ["18"] = {
+        nextCannonLv = 350,
+        expMultiple = 1.0,
+        reward = {{
+            ["1001"] = 30000
+        }},
+        requireItem = {{
+            ["5001"] = 35
+        }},
+        cannonLv = 300
+    },
+    ["32"] = {
+        nextCannonLv = 4000,
+        expMultiple = 1.2,
+        reward = {{
+            ["1001"] = 150000
+        }},
+        requireItem = {{
+            ["5001"] = 500
+        }},
+        cannonLv = 3500
+    },
+    ["33"] = {
+        nextCannonLv = 4500,
+        expMultiple = 1.2,
+        reward = {{
+            ["1001"] = 200000
+        }},
+        requireItem = {{
+            ["5001"] = 600
+        }},
+        cannonLv = 4000
+    },
+    ["34"] = {
+        nextCannonLv = 5000,
+        expMultiple = 1.2,
+        reward = {{
+            ["1001"] = 200000
+        }},
+        requireItem = {{
+            ["5001"] = 800
+        }},
+        cannonLv = 4500
+    },
+    ["31"] = {
+        nextCannonLv = 3500,
+        expMultiple = 1.2,
+        reward = {{
+            ["1001"] = 150000
+        }},
+        requireItem = {{
+            ["5001"] = 400
+        }},
+        cannonLv = 3000
+    },
+    ["19"] = {
+        nextCannonLv = 400,
+        expMultiple = 1.0,
+        reward = {{
+            ["1001"] = 35000
+        }, {
+            ["4042"] = 1
+        }},
+        requireItem = {{
+            ["5001"] = 40
+        }},
+        cannonLv = 350
+    },
+    ["15"] = {
+        nextCannonLv = 200,
+        expMultiple = 1.0,
+        reward = {{
+            ["1001"] = 15000
+        }, {
+            ["4023"] = 1
+        }},
+        requireItem = {{
+            ["5001"] = 25
+        }},
+        cannonLv = 150
+    },
+    ["5"] = {
+        nextCannonLv = 35,
+        expMultiple = 1.0,
+        reward = {{
+            ["1001"] = 2000
+        }},
+        requireItem = {{
+            ["5001"] = 7
+        }},
+        cannonLv = 30
+    },
+    ["41"] = {
+        nextCannonLv = 30000,
+        expMultiple = 1.4,
+        reward = {{
+            ["1001"] = 400000
+        }},
+        requireItem = {{
+            ["5001"] = 3500
+        }},
+        cannonLv = 20000
+    }
+}
